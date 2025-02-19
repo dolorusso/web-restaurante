71866714aa0fdcc939d0382abd7fe884098fcebe
Dolores Russo <drusso@alumnos.exa.unicen.edu.ar>
2025-02-19 00:03:21 -0300

    cambio titulo

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -8,7 +8,7 @@ export const CallToAction = () => {
   return (
     <section className="bg-gradient-to-b from-white  to-orange-100 py-24">
       <div className="container max-w">
-        <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-orange-600 to-[#961903] text-transparent bg-clip-text">VenÍ a conocernos</h2>
+        <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-orange-600 to-[#961903] text-transparent bg-clip-text">Veni a conocernos</h2>
         <p className="text-center text-[20px] leading-[30px] tracking-tight text-[#36120e] mt-5">Haz tu reserva ahora y asegura tu experiencia única en nuestro restaurante. ¡Te esperamos!</p>
         <div className="flex gap-2 mt-10 justify-center">
           <button className="px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight bg-[#961903] text-white">Reserva ahora</button>
